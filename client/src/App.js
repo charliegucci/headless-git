36a5e13448999377a8319408a644c693bbd335f3
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-18 22:49:29 +1000

    React Router Setup # Register Form & useState Hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react';
-import { Navbar } from '../src/components/layout/Navbar';
-import { Landing } from '../src/components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 export default App;
