a180f5a4984c420e15a6fb93e303c82c6bdbf7a9
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-18 12:51:14 +1000

    Add and Delete Profile Experiences

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/Users');
 const { check, validationResult } = require('express-validator');
+
 // @route GET api/profile/me
 // @desc Get current users profile
 // @access Private
@@ -105,6 +106,7 @@ router.post(
 // @route GET api/profile
 // @desc GET all profile
 // @access Public
+
 router.get('/', async (req, res) => {
   try {
     const profiles = await Profile.find().populate('user', ['name', 'avatar']);
@@ -118,6 +120,7 @@ router.get('/', async (req, res) => {
 // @route GET api/profile/user/:user_id
 // @desc GET profile by user ID
 // @access Public
+
 router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
@@ -138,6 +141,7 @@ router.get('/user/:user_id', async (req, res) => {
 // @route DELETE api/profile
 // @desc DELETE profile, user & post
 // @access Private
+
 router.delete('/', auth, async (req, res) => {
   try {
     // Remove Profile
@@ -147,6 +151,72 @@ router.delete('/', auth, async (req, res) => {
     // Remove User
     await User.findOneAndRemove({ _id: req.user.id });
     res.json({ msg: 'User Deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route PUT api/profile/experience
+// @desc Add Profile experience
+// @access Private
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From Date is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc DELETE Experience from profile
+// @access Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // GET Remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
